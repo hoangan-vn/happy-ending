a0b7a0a765c09db000160d2722ebbab4a12d2c09
Nguyễn Hoàng An <an.nh@odh.vn>
2025-10-15 22:59:37 +0700

    init backend project

---
+++ backend/src/main/kotlin/com/happyending/HappyendingApplication.kt
@@ -0,0 +1,11 @@
+package com.happyending
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HappyendingApplication
+
+fun main(args: Array<String>) {
+	runApplication<HappyendingApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/happyending/HappyendingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.happyending
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HappyendingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
